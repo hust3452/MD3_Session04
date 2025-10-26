dad85780d7a73042d1dc5844993cdc999da4d564
hust3452 <hust3452@gmail.com>
2025-10-26 09:54:57 +0700

    Add files via upload

---
+++ 001/src/main/java/ra/mvc/controller/HomeControler.java
@@ -0,0 +1,12 @@
+package ra.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControler {
+@RequestMapping("/home")
+    public String home() {
+    return "home";
+}
+}
